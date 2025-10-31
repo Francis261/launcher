3089bc0ecf88881db3d4e3ddbb2bd49405dc9bf4
Francis261 <francisdominic986532@gmail.com>
2025-10-31 22:03:30 +0100

    Add MainActivity class for launcher app

---
+++ app/src/main/java/com/example/launcher/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.launcher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
